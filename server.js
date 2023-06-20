3ea3bbf4ef6ac45b756e058a6ecc26c5839edd09
Thabangx10 <Thabangx12@gmail.com>
2023-06-20 14:13:50 +0200

    Ading to github

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-// This file will contain the code for your JSON server
+document.addEventListener("DOMContentLoaded", () => {
+  const tbody = document.getElementById("tbody");
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+  // Fetch function
+  fetch("/db.json")
+      .then((res) => res.json())
+      .then((json) => {
+          json.user.forEach((data) => {
+              tbody.append(createTableRow(data));
+          });
+      })
+      .catch((error) => {
+          console.log("Error fetching data:", error);
+      });
 
-server.use(middlewares);
-server.use(router);
-const port = 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+  // Create table row
+  function createTableRow({ profile, name, email, status, role }) {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+          <td class="px-6 py-4 whitespace-nowrap">
+              <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                      <img src="${profile}" class="h-10 w-10 rounded-full" alt="">
+                  </div>
+                  <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">${name}</div>
+                      <div class="text-sm text-gray-500">${email}</div>
+                  </div>
+              </div>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                  ${status}
+              </span>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+              <span class="text-sm text-gray-500">${role}</span>
+          </td>
+      `;
+      return tr;
+  }
 });
